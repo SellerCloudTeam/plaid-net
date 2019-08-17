e10f13275ffa6c9b42a902314d8a416cd436079d
Stan <stanimir@sellercloud.com>
2019-08-17 10:40:01 +0300

    v1004

--- src/Plaid.NET/src/Plaid/Institution/GetOptions.cs
+++ src/Plaid.NET/src/Plaid/Institution/GetOptions.cs
@@ -4,13 +4,7 @@ namespace Acklann.Plaid.Institution
 {
     public class GetOptions
     {
-        [JsonProperty("products")]
-        public string[] Products { get; set; }
-
         [JsonProperty("include_optional_metadata")]
         public bool IncludeOptionalMetadata { get; set; }
-
-        [JsonProperty("country_codes")]
-        public string[] CountryCodes { get; set; }
     }
 }
